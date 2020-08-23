60b1f038dedb1def313b3ace9e18c79a3036c8a5
Arunabh Das <arunabhdas@gmail.com>
2020-08-22 22:11:54 -0400

    Updated model.py

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 
 class Employee:
     
+    raise_amount = 1.04 
+
     def __init__(self, firstname, lastname, salary):
         self.firstname = firstname
         self.lastname = lastname
@@ -8,6 +10,11 @@ class Employee:
         self.email = firstname + "." + lastname + "@company.com" 
 
 
+    def fullname(self):
+        return '{} {}'.format(self.firstname, self.lastname)
+
+    def apply_raise(self):
+        self.salary = int(self.salary * Employee.raise_amount)
 
 emp1 = Employee('Adam', 'Smith', 50000)
 emp2 = Employee('Paul', 'Samuelson', 100000)
@@ -16,5 +23,10 @@ print(emp1.email)
 
 print(emp2.email)
 
-print(' {} {} '.format(emp1.firstname, emp1.lastname))
+print(emp1.fullname())
+print(emp1.salary)
+
+emp1.apply_raise()
+
+print(emp1.salary)
 
