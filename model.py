4a67004e23a701a0cb621b28ea34e37a74b7f59e
Arunabh Das <arunabhdas@gmail.com>
2020-09-04 00:36:00 -0400

    Added classmethod constructor

--- model.py
+++ model.py
@@ -21,11 +21,31 @@ class Employee:
     def set_raise_amt(cls, amount):
         cls.raise_amt = amount
 
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, sal = emp_str.split('-')
+        return cls(first, last, sal)
+        
+
 emp1 = Employee('Adam', 'Smith', 50000)
 emp2 = Employee('Paul', 'Samuelson', 100000)
 
-Employee.set_raise_amt(1.05)
 
-print(Employee.raise_amt)
-print(emp1.raise_amt)
-print(emp2.raise_amt)
+
+# Employee.set_raise_amt(1.05)
+
+# print(Employee.raise_amt)
+# print(emp1.raise_amt)
+# print(emp2.raise_amt)
+
+
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'Jane-Doe-90000'
+
+
+new_emp_1 = Employee.from_string(emp_str_1)
+
+print(new_emp_1.email)
+print(new_emp_1.salary)
+
